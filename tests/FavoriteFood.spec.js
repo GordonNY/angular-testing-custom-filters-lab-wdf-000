3330a24ce5fbe9e5bbcec62e601599cac870f77d
GordonNY <gordonsdev@gmail.com>
2016-08-17 15:08:39 -0400

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,22 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+	it('should filter on favorite food correctly', function () {
+		var mockedList = [{
+			name: 'name1',
+			favoriteFood: 'food1'
+		}, {
+			name: 'name2',
+			favoriteFood: 'food2'
+		}, {
+			name: 'name3',
+			favoriteFood: 'food3'
+		}];
+
+		var results = $filter('favoriteFood')(mockedList, 'food1');
+
+		expect(results.length).toBe(1);
+		expect(results[0].name).toBe('name1');
+	});
 	
 });
